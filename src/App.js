f48495d820802710906c7560f153dc76329365ec
Grzegorz Domaradzki <gd@tcz.pl>
2019-02-23 19:29:16 +0100

    tasks 3-5 solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        name: "John",
+        surname: "Doe",
+        phone: "123-456-789",
+        isFavorite: false
+      },
+      {
+        id: 2,
+        name: "Steve",
+        surname: "Stevens",
+        phone: "987-654-321",
+        isFavorite: true
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map(user => (
+              <tr key={user.id} className={user.isFavorite && "Favorite"}>
+                <td>{user.name}</td>
+                <td>{user.surname}</td>
+                <td>{user.phone}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
